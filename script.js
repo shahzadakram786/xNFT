284910c6e50a84e5cd0538def45185795ea04440
shahzad akram <alyakram786@gmail.com>
2024-03-22 16:00:22 +0500

    working to make a loop on table to make it dynamic

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ const tables_data = [{
     floor_nameB: 'ETH', 
     floor_change: '-2.6%',
     Volume:'1,181' ,
-    volumneB:ETH,
+    volumneB:'ETH',
     volumne_change: '-12.3%',
     items:'19.5K',
     Owners:'11.6K'
@@ -66,8 +66,46 @@ const tables_data = [{
     floor_nameB: 'ETH', 
     floor_change: '+0.1%',
     Volume:'820.2' ,
-    volumneB:ETH,
+    volumneB:'ETH',
     volumne_change: '+614%',
     items:'10K',
     Owners:'4.4K'   
+},
+{
+    id : 3,
+    Image: 'images/',
+    collection: 'Nobody',
+    floor_name: 0.605,
+    floor_nameB: 'ETH', 
+    floor_change: '-7.4%',
+    Volume:'645.2' ,
+    volumneB:'ETH',
+    volumne_change: '-65.9%',
+    items:'10K',
+    Owners:'4.7K'   
 }]
+
+
+const tBody = document.getElementsByTagName('tbody')
+
+for(let i = 0 ; i < tables_data.length; i++) {
+    let tableData = tables_data[i];
+
+    tBody.innerHTML += 
+   `
+   <tr class="table_div_2">
+                              <td>${tableData.id}</td>
+                              <td class="td_img_p"><img src="./images/table icons/div.sc-aXZVg.png" alt="" id="T_img1" class="t_imges">
+                               <p>MutantApeYachtClub</p> </td>
+                              <td>4.31 <br> ETH</td>
+                              <td>-2.6%</td>
+                              <td>1,181 <br> ETH</td>
+                              <td>-12.3%</td>
+                              <td>19.5K</td>
+                              <td>11.6K</td>
+                           </tr>
+
+   `
+
+}
+
