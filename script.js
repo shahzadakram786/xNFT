17a3958106e33ec3ddf19efd03671a7aca82b5c2
shahzad akram <alyakram786@gmail.com>
2024-03-26 16:04:13 +0500

    debugging the code for cards 2

--- script.js
+++ script.js
@@ -1,334 +1,398 @@
-let num = document.querySelector("#First_num")
- 
+let num = document.querySelector("#First_num");
 
-
-
-const first_numbers = [{
-number: '10k' ,
-title: 'Artwork'
-},
-{
-    number: '32k',
-    title: 'Auction'
-},
-{
-    number: '42k',
-    title: 'Artist'
-}];
+const first_numbers = [
+  {
+    number: "10k",
+    title: "Artwork",
+  },
+  {
+    number: "32k",
+    title: "Auction",
+  },
+  {
+    number: "42k",
+    title: "Artist",
+  },
+];
 
 // this is for the first number and there titles on section 1
 
-
 for (let i = 0; i < first_numbers.length; i++) {
-let fistData = first_numbers[i];
+  let fistData = first_numbers[i];
 
-    console.log("thies +> ",fistData.number)
-    console.log("thies +> ",fistData.title)
+  console.log("thies +> ", fistData.number);
+  console.log("thies +> ", fistData.title);
 
-    num.innerHTML +=  
-    `
+  num.innerHTML += `
     <div class="for-num">
     <span class="spanNum-btn">
      <h2>${fistData.number}</h2>
      <p>${fistData.title}</p>
     </span>
     
-    ${i !== first_numbers.length - 1 ? '<div class="line" id="one-line"></div>' : ''} 
-</div>`
-
-
+    ${
+      i !== first_numbers.length - 1
+        ? '<div class="line" id="one-line"></div>'
+        : ""
+    } 
+</div>`;
 }
-
-
-
 
 // section 2 data from here
 
-const tBody = document.querySelector('.tbody')
+const tBody = document.querySelector(".tbody");
 
-
-const tables_data = [{
-    id : 1,
-    Image: './images/table icons/1.png',
-    collection: 'MutantApeYachtClub',
+const tables_data = [
+  {
+    id: 1,
+    Image: "./images/table icons/1.png",
+    collection: "MutantApeYachtClub",
     floor_name: 4.31,
-    floor_nameB: 'ETH', 
-    floor_change: '-2.6%',
-    Volume:'1,181' ,
-    volumneB:'ETH',
-    volumne_change: '-12.3%',
-    items:'19.5K',
-    Owners:'11.6K'
-},
-{
-    id : 2,
-    Image: './images/table icons/2.png',
-    collection: 'Azuki',
+    floor_nameB: "ETH",
+    floor_change: "-2.6%",
+    Volume: "1,181",
+    volumneB: "ETH",
+    volumne_change: "-12.3%",
+    items: "19.5K",
+    Owners: "11.6K",
+  },
+  {
+    id: 2,
+    Image: "./images/table icons/2.png",
+    collection: "Azuki",
     floor_name: 6.99,
-    floor_nameB: 'ETH', 
-    floor_change: '+0.1%',
-    Volume:'820.2' ,
-    volumneB:'ETH',
-    volumne_change: '+614%',
-    items:'10K',
-    Owners:'4.4K'   
-},
-{
-    id : 3,
-    Image: './images/table icons/3.png',
-    collection: 'Nobody',
+    floor_nameB: "ETH",
+    floor_change: "+0.1%",
+    Volume: "820.2",
+    volumneB: "ETH",
+    volumne_change: "+614%",
+    items: "10K",
+    Owners: "4.4K",
+  },
+  {
+    id: 3,
+    Image: "./images/table icons/3.png",
+    collection: "Nobody",
     floor_name: 0.605,
-    floor_nameB: 'ETH', 
-    floor_change: '-7.4%',
-    Volume:'645.2' ,
-    volumneB:'ETH',
-    volumne_change: '-65.9%',
-    items:'10K',
-    Owners:'4.7K'   
-}
-,
-{
-    id : 4,
-    Image: './images/table icons/4.png',
-    collection: 'Pudgy Penguins',
+    floor_nameB: "ETH",
+    floor_change: "-7.4%",
+    Volume: "645.2",
+    volumneB: "ETH",
+    volumne_change: "-65.9%",
+    items: "10K",
+    Owners: "4.7K",
+  },
+  {
+    id: 4,
+    Image: "./images/table icons/4.png",
+    collection: "Pudgy Penguins",
     floor_name: 18.5,
-    floor_nameB: 'ETH', 
-    floor_change: '+8.9%',
-    Volume:'563' ,
-    volumneB:'ETH',
-    volumne_change: '+39.9%',
-    items:'8.9K',
-    Owners:'4.6K'   
-}
-,
-{
-    id : 5,
-    Image: './images/table icons/5.png',
-    collection: 'Farm Land by Pixels',
+    floor_nameB: "ETH",
+    floor_change: "+8.9%",
+    Volume: "563",
+    volumneB: "ETH",
+    volumne_change: "+39.9%",
+    items: "8.9K",
+    Owners: "4.6K",
+  },
+  {
+    id: 5,
+    Image: "./images/table icons/5.png",
+    collection: "Farm Land by Pixels",
     floor_name: 1.2,
-    floor_nameB: 'ETH', 
-    floor_change: '-36%',
-    Volume:'496.6' ,
-    volumneB:'ETH',
-    volumne_change: '+297.7%',
-    items:'5K',
-    Owners:'2.1K'   
-}
-,
-{
-    id : 6,
-    Image: './images/table icons/6.png',
-    collection: 'TENJIN',
+    floor_nameB: "ETH",
+    floor_change: "-36%",
+    Volume: "496.6",
+    volumneB: "ETH",
+    volumne_change: "+297.7%",
+    items: "5K",
+    Owners: "2.1K",
+  },
+  {
+    id: 6,
+    Image: "./images/table icons/6.png",
+    collection: "TENJIN",
     floor_name: 0.541,
-    floor_nameB: 'ETH', 
-    floor_change: '+11.5%',
-    Volume:'489.4' ,
-    volumneB:'ETH',
-    volumne_change: '+126.5%',
-    items:'8K',
-    Owners:'4.2K'   
-},
-{
-    id : 7,
-    Image: './images/table icons/7.png',
-    collection: 'DeGods',
+    floor_nameB: "ETH",
+    floor_change: "+11.5%",
+    Volume: "489.4",
+    volumneB: "ETH",
+    volumne_change: "+126.5%",
+    items: "8K",
+    Owners: "4.2K",
+  },
+  {
+    id: 7,
+    Image: "./images/table icons/7.png",
+    collection: "DeGods",
     floor_name: 2.999,
-    floor_nameB: 'ETH', 
-    floor_change: '-5.3%',
-    Volume:'437' ,
-    volumneB:'ETH',
-    volumne_change: '-47.2%',
-    items:'9K',
-    Owners:'2.8K'   
-},
-{
-    id : 8,
-    Image: './images/table icons/8.png',
-    collection: 'Pillheads NFT',
+    floor_nameB: "ETH",
+    floor_change: "-5.3%",
+    Volume: "437",
+    volumneB: "ETH",
+    volumne_change: "-47.2%",
+    items: "9K",
+    Owners: "2.8K",
+  },
+  {
+    id: 8,
+    Image: "./images/table icons/8.png",
+    collection: "Pillheads NFT",
     floor_name: 0.145,
-    floor_nameB: 'ETH', 
-    floor_change: '+107.4%',
-    Volume:'220.1' ,
-    volumneB:'ETH',
-    volumne_change: '+438.7%',
-    items:'6.7K',
-    Owners:'2K'   
-},
-{
-    id : 9,
-    Image: './images/table icons/9.png',
-    collection: 'Lil Pudgys',
+    floor_nameB: "ETH",
+    floor_change: "+107.4%",
+    Volume: "220.1",
+    volumneB: "ETH",
+    volumne_change: "+438.7%",
+    items: "6.7K",
+    Owners: "2K",
+  },
+  {
+    id: 9,
+    Image: "./images/table icons/9.png",
+    collection: "Lil Pudgys",
     floor_name: 1.739,
-    floor_nameB: 'ETH', 
-    floor_change: '+3%',
-    Volume:'201.7' ,
-    volumneB:'ETH',
-    volumne_change: '-52.5%',
-    items:'21.5K',
-    Owners:'8K'   
-}
-,
-{
-    id : 10,
-    Image: './images/table icons/10.png',
-    collection: 'Bored Ape Yacht Club',
+    floor_nameB: "ETH",
+    floor_change: "+3%",
+    Volume: "201.7",
+    volumneB: "ETH",
+    volumne_change: "-52.5%",
+    items: "21.5K",
+    Owners: "8K",
+  },
+  {
+    id: 10,
+    Image: "./images/table icons/10.png",
+    collection: "Bored Ape Yacht Club",
     floor_name: 24.29,
-    floor_nameB: 'ETH', 
-    floor_change: '-2.8%',
-    Volume:'142.3' ,
-    volumneB:'ETH',
-    volumne_change: '-7.1%',
-    items:'10K',
-    Owners:'5.7K'   
-},
-{
-    id : 11,
-    Image: './images/table icons/11.png',
-    collection: 'BoredApekennelClub',
+    floor_nameB: "ETH",
+    floor_change: "-2.8%",
+    Volume: "142.3",
+    volumneB: "ETH",
+    volumne_change: "-7.1%",
+    items: "10K",
+    Owners: "5.7K",
+  },
+  {
+    id: 11,
+    Image: "./images/table icons/11.png",
+    collection: "BoredApekennelClub",
     floor_name: 1.28,
-    floor_nameB: 'ETH', 
-    floor_change: '-3%' ,
-    Volume:'122.9' ,
-    volumneB:'ETH',
-    volumne_change: '+265.1%',
-    items:'9.6K',
-    Owners:'5.2K'   
-}]
+    floor_nameB: "ETH",
+    floor_change: "-3%",
+    Volume: "122.9",
+    volumneB: "ETH",
+    volumne_change: "+265.1%",
+    items: "9.6K",
+    Owners: "5.2K",
+  },
+];
 
-
-
-for(let i = 0 ; i < tables_data.length; i++) {
-    let tableData = tables_data[i];
-// console.log("rBLE", tableData.id)
-
+for (let i = 0; i < tables_data.length; i++) {
+  let tableData = tables_data[i];
+  // console.log("rBLE", tableData.id)
 
   let flrChange = parseInt(tableData.floor_change);
   let volChange = parseInt(tableData.volumne_change);
 
+  console.log("what 1", flrChange);
+  console.log("what", volChange);
 
-console.log("what 1",flrChange );
-console.log("what",volChange);
-
-    tBody.innerHTML += 
-   `
+  tBody.innerHTML += `
    <tr class="table_div_2">
       <td class="ids">${tableData.id}</td>
-      <td class="td_img_p"><img src="${tableData.Image}" alt="" id="T_img1" class="t_imges">
+      <td class="td_img_p"><img src="${
+        tableData.Image
+      }" alt="" id="T_img1" class="t_imges">
        <p>${tableData.collection}</p> </td>
       <td>${tableData.floor_name} <br> ${tableData.floor_nameB}</td>
-      <td class="${flrChange > 0 ? 'positive' : 'negative'}">${tableData.floor_change}</td>
+      <td class="${flrChange > 0 ? "positive" : "negative"}">${
+    tableData.floor_change
+  }</td>
       <td>${tableData.Volume} <br> ${tableData.volumneB}</td>
-      <td class="${volChange > 0 ? 'positive' : 'negative'}">${tableData.volumne_change}</td>
+      <td class="${volChange > 0 ? "positive" : "negative"}">${
+    tableData.volumne_change
+  }</td>
       <td>${tableData.items}</td>
       <td>${tableData.Owners}</td>
     </tr>
 
-   `
-// now i am working on a condition in which i can create a fucntion for positive and negative numbers
+   `;
+  // now i am working on a condition in which i can create a fucntion for positive and negative numbers
 
+  // if(flrChange > 0 && volChange > 0 ){
+  //     console.log("positive")
+  //     console.log("positvive => ",tableData.floor_change);
+  //     // flrChange.style.color = "green";
 
+  // }else{
+  //     console.log("negative")
+  //     // volChange.style.color ="red";
 
-// if(flrChange > 0 && volChange > 0 ){
-//     console.log("positive")
-//     console.log("positvive => ",tableData.floor_change);
-//     // flrChange.style.color = "green";
-
-// }else{
-//     console.log("negative")
-//     // volChange.style.color ="red";
-   
-// }
+  // }
 }
 
+// now working on section 3  cards part has start from here
 
-
-
-// now working on section 3  cards part has start from here 
-
-const collections = [{
-    nftImg:"./images/cards images/Image.png" ,
-    time : "05:12:07:45",
-    title : "Hamlet Contemplates ...",
-    coin:"BSC",
-    pImg : "./images/cards images/Background.png",
+const collections = [
+  {
+    nftImg: "./images/cards images/Image.png",
+    time: "05:12:07:45",
+    title: "Hamlet Contemplates ...",
+    coin: "BSC",
+    pImg: "./images/cards images/Background.png",
     Creatior: "Creator",
     CurrentBid: "Current Bid",
     Name: "SalvadorDali",
-    price : 4.89,
-    curr : "ETH"
-},
-{
-    nftImg:"./images/cards images/IMG.png" ,
-    time : "05:12:07:45",
-    title : "Hamlet Contemplates ...",
-    coin:"BSC",
-    pImg : "./images/cards images/Background (1).png",
+    price: 4.89,
+    curr: "ETH",
+  },
+  {
+    nftImg: "./images/cards images/IMG.png",
+    time: "05:12:07:45",
+    title: "Hamlet Contemplates ...",
+    coin: "BSC",
+    pImg: "./images/cards images/Background (1).png",
     Creatior: "Creator",
     CurrentBid: "Current Bid",
     Name: "SalvadorDali",
-    price : 4.89,
-    curr : "ETH"
-},
-{
-    nftImg:"./images/cards images/IMG (1).png" ,
-    time : "05:12:07:45",
-    title : "Hamlet Contemplates ...",
-    coin:"BSC",
-    pImg : "./images/cards images/Background (2).png",
+    price: 4.89,
+    curr: "ETH",
+  },
+  {
+    nftImg: "./images/cards images/IMG (1).png",
+    time: "05:12:07:45",
+    title: "Hamlet Contemplates ...",
+    coin: "BSC",
+    pImg: "./images/cards images/Background (2).png",
     Creatior: "Creator",
     CurrentBid: "Current Bid",
     Name: "SalvadorDali",
-    price : 4.89,
-    curr : "ETH"
-},{
-    nftImg:"./images/cards images/Image.png" ,
-    time : "05:12:07:45",
-    title : "Hamlet Contemplates ...",
-    coin:"BSC",
-    pImg : "./images/cards images/Background.png",
+    price: 4.89,
+    curr: "ETH",
+  },
+  {
+    nftImg: "./images/cards images/Image.png",
+    time: "05:12:07:45",
+    title: "Hamlet Contemplates ...",
+    coin: "BSC",
+    pImg: "./images/cards images/Background.png",
     Creatior: "Creator",
     CurrentBid: "Current Bid",
     Name: "SalvadorDali",
-    price : 4.89,
-    curr : "ETH",
-     curr : "ETH"
-},{
-    nftImg:"./images/cards images/IMG (1).png" ,
-    time : "05:12:07:45",
-    title : "Hamlet Contemplates ...",
-    coin:"BSC",
-    pImg : "./images/cards images/Background (2).png",
+    price: 4.89,
+    curr: "ETH",
+    curr: "ETH",
+  },
+  {
+    nftImg: "./images/cards images/IMG (1).png",
+    time: "05:12:07:45",
+    title: "Hamlet Contemplates ...",
+    coin: "BSC",
+    pImg: "./images/cards images/Background (2).png",
     Creatior: "Creator",
     CurrentBid: "Current Bid",
     Name: "SalvadorDali",
-    price : 4.89,
-    curr : "ETH"
-    },{
-        nftImg:"./images/cards images/Image.png" ,
-        time : "05:12:07:45",
-        title : "Hamlet Contemplates ...",
-        coin:"BSC",
-        pImg : "./images/cards images/Background.png",
-        Creatior: "Creator",
-        CurrentBid: "Current Bid",
-        Name: "SalvadorDali",
-        price : 4.89,
-        curr : "ETH"
-    }]
+    price: 4.89,
+    curr: "ETH",
+  },
+  {
+    nftImg: "./images/cards images/Image.png",
+    time: "05:12:07:45",
+    title: "Hamlet Contemplates ...",
+    coin: "BSC",
+    pImg: "./images/cards images/Background.png",
+    Creatior: "Creator",
+    CurrentBid: "Current Bid",
+    Name: "SalvadorDali",
+    price: 4.89,
+    curr: "ETH",
+  },
+];
 
+const collections_2 = [
+  {
+    nftImg: "./images/cards images/ImageS2.png",
+    time: "05:12:07:45",
+    title: "Hamlet Contemplates ...",
+    coin: "BSC",
+    pImg: "./images/cards images/Background.png",
+    Creatior: "Creator",
+    CurrentBid: "Current Bid",
+    Name: "SalvadorDali",
+    price: 4.89,
+    curr: "ETH",
+  },
+  {
+    nftImg: "./images/cards images/IMGS2.png",
+    time: "05:12:07:45",
+    title: "Hamlet Contemplates ...",
+    coin: "BSC",
+    pImg: "./images/cards images/Background (1).png",
+    Creatior: "Creator",
+    CurrentBid: "Current Bid",
+    Name: "SalvadorDali",
+    price: 4.89,
+    curr: "ETH",
+  },
+  {
+    nftImg: "./images/cards images/IMG (1)S2.png",
+    time: "05:12:07:45",
+    title: "Hamlet Contemplates ...",
+    coin: "BSC",
+    pImg: "./images/cards images/Background (2).png",
+    Creatior: "Creator",
+    CurrentBid: "Current Bid",
+    Name: "SalvadorDali",
+    price: 4.89,
+    curr: "ETH",
+  },
+  {
+    nftImg: "./images/cards images/IMGS2.png",
+    time: "05:12:07:45",
+    title: "Hamlet Contemplates ...",
+    coin: "BSC",
+    pImg: "./images/cards images/Background.png",
+    Creatior: "Creator",
+    CurrentBid: "Current Bid",
+    Name: "SalvadorDali",
+    price: 4.89,
+    curr: "ETH",
+    curr: "ETH",
+  },
+  {
+    nftImg: "./images/cards images/IMG (1)S2.png",
+    time: "05:12:07:45",
+    title: "Hamlet Contemplates ...",
+    coin: "BSC",
+    pImg: "./images/cards images/Background (2).png",
+    Creatior: "Creator",
+    CurrentBid: "Current Bid",
+    Name: "SalvadorDali",
+    price: 4.89,
+    curr: "ETH",
+  },
+  {
+    nftImg: "./images/cards images/ImageS2.png",
+    time: "05:12:07:45",
+    title: "Hamlet Contemplates ...",
+    coin: "BSC",
+    pImg: "./images/cards images/Background.png",
+    Creatior: "Creator",
+    CurrentBid: "Current Bid",
+    Name: "SalvadorDali",
+    price: 4.89,
+    curr: "ETH",
+  },
+];
 
-
-function firstFunction(){
-
-let cards = document.querySelector(".slider");
-
-for(let i = 0 ; i < collections.length; i++) {
-    let collectionData = collections[i];
+function firstFunction(cardData, cards) {
+  for (let i = 0; i < cardData.length; i++) {
+    let collectionData = cardData[i];
 
     console.log("Working");
 
-    cards.innerHTML += 
-    `
+    cards.innerHTML += `
     <div class="cards">
                 
             
@@ -350,60 +414,56 @@ for(let i = 0 ; i < collections.length; i++) {
                 </div>
 
                 </div>
-    `
-
-
+    `;
+  }
 }
+let cards = document.querySelector(".slider_1");
 
-}
-firstFunction()
+firstFunction(collections, cards);
 
 // Select the buttons
 
-let slides = document.querySelectorAll(".cards")
+function firstSlides(slides) {
+  var count = 0;
 
-var count = 0;
-slides.forEach(
-    (slide, index) => {
-        slide.style.left = `${index * 33}%`
+  slides.forEach((slide, index) => {
+    slide.style.left = `${index * 33}%`;
+  });
 
-    })
-
-
-
-
-    let next = () => {
-        if (count == 3) {
-            count = -1;
-        }
-        count++;
-        slideImage()
-    
-    
+  // Define next and back functions outside firstSlides for global access
+  function next() {
+    if (count == 3) {
+      count = 0; // Reset to first slide (alternative: -1 for smooth transition)
     }
-    
-    let back = () => {
-        if (count == 2) {
-            count = 4;
-        }
-        count--;
-        slideImage()
-    
+    count++;
+    slideImage();
+  }
+
+  function back() {
+    if (count == 0) {
+      count = slides.length - 1; // Reset to last slide
     }
+    count--;
+    slideImage();
+  }
 
-    const slideImage = () => {
+  const slideImage = () => {
+    slides.forEach((slide) => {
+      slide.style.transform = `translateX(-${count * 100}%)`;
+    });
+  };
+}
 
-        slides.forEach(
-            (slide) => {
-                slide.style.transform = `translateX(-${count * 100}%)`;
-            })
-    
-    }
+let slides = document.querySelectorAll(".cards");
+firstSlides(slides);
+
+  
 
 
 
 
-let cards2 = document.querySelector(".slider_2")
 
-cards2.innerHTML += firstFunction()
-// i want to add the first function just nft img should be changed
+let cards2 = document.querySelector(".slider_2");
+firstFunction(collections_2, cards2);
+let slides2 = document.querySelectorAll(".cards2");
+firstSlides(slides2)
